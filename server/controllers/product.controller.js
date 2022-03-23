1840484bff83f0fed57b57cbb4133b7584fcc59e
Lauren Odalen <97255159+LaurenOdalen@users.noreply.github.com>
2022-03-23 07:08:19 -0600

    Update product.controller.js

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -9,11 +9,29 @@ module.exports.index = (req, res) => {
 module.exports.createProduct = (req, res) =>{
     Product.create(req.body)
     .then(newProduct => res.json(newProduct))
-    .catch(err => res.json({message: 'Something went wrong', error:err}))
+    .catch(err => res.status(400).json(err))
 }
 
 module.exports.findAllProducts = (req, res)=> {
     Product.find()
     .then(allProducts => res.json(allProducts))
-    .catch(err => res.json({message: "Something went wrong", error: err}));
+    .catch(err => res.status(400).json(err));
+}
+
+module.exports.findOneProduct = (req, res) => {
+    Product.findOne({_id: req.params.id})
+    .then(oneProduct => res.json(oneProduct))
+    .catch(err => res.status(400).json(err))
+}
+
+module.exports.updateOneProduct = (req, res) => {
+    Product.findOneAndUpdate({_id: req.params.id}, req.body, {new:true})
+    .then(updateProduct => res.json(updateProduct))
+    .catch(err => res.status(400).json(err))
+}
+
+module.exports.deleteProduct = (req, res) => {
+    Product.deleteOne({_id: req.params.id})
+    .then(deleteOneProduct => res.json(deleteOneProduct))
+    .catch(err => res.status(400).json(err))
 }
