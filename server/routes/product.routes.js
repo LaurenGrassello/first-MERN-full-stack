22fe54ff73a757245c76778a675c22b497f55fe0
Lauren Odalen <97255159+LaurenOdalen@users.noreply.github.com>
2022-03-23 07:11:14 -0600

    Update product.routes.js

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -4,8 +4,8 @@ module.exports = app => {
     app.get('/api', ProductController.index);
     app.post('/api/product', ProductController.createProduct);
     app.get('/api/product', ProductController.findAllProducts);
-    // app.get('/api/product/:id', ProductController.findOneProduct);
-    // app.put('/api/product/:id', ProductController.updateOneProduct);
-    // app.delete('/api/product/:id', ProductController.deleteProduct);
+    app.get('/api/product/:id', ProductController.findOneProduct);
+    app.put('/api/product/:id', ProductController.updateOneProduct);
+    app.delete('/api/product/:id', ProductController.deleteProduct);
 
 }
